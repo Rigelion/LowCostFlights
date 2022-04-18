9494f203004f4358fdbf9f4c71e240015156cba7
Rigelion <dino.habibovic@gmail.com>
2022-04-18 16:54:16 +0200

    Update Infrastructure proj, LowCostFlights sln Update program, register services and automapper, add currencyRates as singleton

--- WebUI/Program.cs
+++ WebUI/Program.cs
@@ -3,22 +3,37 @@ using Infrastructure.Interfaces;
 using Infrastructure.Services;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Extensions.DependencyInjection;
 using Models.Amadeus;
+using Models.CurrencyConverter;
 using WebUI.Data;
 using WebUI.ViewModels;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Get currency rates
+var currencyService = new CurrencyConverterService(builder.Configuration.GetSection(nameof(CurrencyConverterConfiguration)).Get<CurrencyConverterConfiguration>());
+
+var currencyRates = await currencyService.GetCurrencyRates();
+
+builder.Services.AddSingleton(currencyRates);
+
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
-builder.Services.AddSingleton<IIataService, IataService>();
 builder.Services.AddSingleton<AmadeusConfiguration>((serviceProvider) => builder.Configuration.GetSection(nameof(AmadeusConfiguration)).Get<AmadeusConfiguration>());
+//builder.Services.AddSingleton<CurrencyConverterConfiguration>((serviceProvider) => builder.Configuration.GetSection(nameof(CurrencyConverterConfiguration)).Get<CurrencyConverterConfiguration>());
+
+builder.Services.AddSingleton<IIataService, IataService>();
+
 builder.Services.AddScoped<FlightOfferViewModel>();
 builder.Services.AddScoped<IAmadeusService, AmadeusService>();
+builder.Services.AddScoped<ICacheService, CacheService>();
+
 builder.Services.AddTransient<IValidator<FlightOfferViewModel>, FlightRequestViewModelValidator>();
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
