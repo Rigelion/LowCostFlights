05f124dbff7f4430d24de208b0a758f46084270d
Rigelion <dino.habibovic@gmail.com>
2022-04-23 11:02:49 +0200

    Add ICurrencyConverterService Rework CurrencyConverterService         Add caching for CurrencyRates         Add logger         Add default rates to class, remove them from appsettings         Register it as scoped Add ICurrencyConverterService to FlightOfferViewModel Add parameter to mapping, rework CurrencyResolvers to work with mapping parameter Actually add TTL for CacheService, 5 min

--- Infrastructure/Services/CurrencyConverterService.cs
+++ Infrastructure/Services/CurrencyConverterService.cs
@@ -12,28 +12,43 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Services;
 
-public class CurrencyConverterService
+public class CurrencyConverterService : ICurrencyConverterService
 {
+
+    private readonly CurrencyRates DEFAULT_CURRENCY_RATES = new() { EurToHrkRate = 7.56m, EurToUsdRate = 1.07m };
+
+
     private readonly CurrencyConverterConfiguration _currencyConverterConfiguration;
     private readonly RestClient _restClient;
+    private readonly ILogger<CurrencyConverterService> _logger;
+    private readonly ICacheService _cacheService;
 
-    public CurrencyConverterService(CurrencyConverterConfiguration currencyConverterConfiguration)
+    public CurrencyConverterService(CurrencyConverterConfiguration currencyConverterConfiguration, ILogger<CurrencyConverterService> logger, ICacheService cacheService)
     {
         _currencyConverterConfiguration = currencyConverterConfiguration;
         _restClient = new(_currencyConverterConfiguration.BaseUrl);
+        _logger = logger;
+        _cacheService = cacheService;
     }
 
-    public async Task<CurrencyRates?> GetCurrencyRates()
+    public async Task<CurrencyRates> GetCurrencyRates()
+    {
+        return await _cacheService.GetFromCacheAndCache(nameof(CurrencyRates), () => RequestCurrencyRates());
+    }
+
+    private async Task<CurrencyRates> RequestCurrencyRates()
     {
         var request = new RestRequest("convert", Method.Get);
 
         request.AddCurrencyConversionParameters(_currencyConverterConfiguration.ApiKey);
 
         var response = await _restClient.ExecuteAsync(request);
-        
-        if(!response.IsSuccessful)
+
+        if (!response.IsSuccessful)
         {
-            return null!;
+            _logger.LogError("Currency Rates API response was not successful! {StatusCode} {@ErrorException}", response.StatusCode, response.ErrorException);
+            _logger.LogInformation("Returning default currency rates {@CurrencyRates}", DEFAULT_CURRENCY_RATES);
+            return DEFAULT_CURRENCY_RATES;
         }
 
         return JsonConvert.DeserializeObject<CurrencyRates>(response.Content!)!;
